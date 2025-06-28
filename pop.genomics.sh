6e8019ff12a72901c066324eb65df7d459b7e2f6
Fernando Hernandez <127550236+fernandoh33@users.noreply.github.com>
2025-06-28 11:21:04 -0600

    Update pop.genomics.sh

--- pop.genomics.sh
+++ pop.genomics.sh
@@ -27,14 +27,30 @@ realSFS saf2theta geno.files/$chr.all.samples.w.invariants.0.1.saf.idx -sfs sfs.
 thetaStat do_stat theta.files/$chr.w.invariants.0.1.thetas.idx -win 100000 -step 100000 -outnames theta.files/$chr.theta.w100.s100;
 done;
 
-#Creating genotype likelihoods including only snps for population genomics, these files can be very large too
-angsd -bam bam.list -fai $REF.fai -ref $REF -nInd 100 -minInd 10 -doMajorMinor 1 -doPost 1 -doMaf 1 -doGlf 2 -out all.samples.snps.maf0.01.minInd0.1 -gl 2 -minMapQ 30 -minQ 20 -minMaf 0.01 -SNP_pval 1e-6 -nThreads 8 -baq 1;
+#Creating genotype likelihoods including only snps for population genomics
+for chr in $(cat salvinia.chromosomes);
+do angsd -bam bam.list -fai $REF.fai -ref $REF -nInd 100 -minInd 10 -doMajorMinor 1 -doPost 1 -doMaf 1 -doGlf 2 -out geno.files/$chr.all.samples.snps.maf0.01.minInd0.1 -gl 2 -minMapQ 30 -minQ 20 -minMaf 0.01 -SNP_pval 1e-6 -nThreads 8 -baq 1;
+done;
 
-#Ideally snps should be filtered by low LD for population genomic analyses, though in this case all samples seems to be a single clone with very low diversity, so we are using all snps
+#merge beagle files, the code is useful to filter beagle files using a list of positions, in this case 4-fold degenerate sites
+#extract the header from any chromosome beagle file, for example Chr_10
+zcat geno.files/Chr_10.all.samples.snps.maf0.01.minInd0.1.beagle.gz | head -1 > header.beagle
+#filter the beagle files or just uncompressed them to merge
+for chr in $(cat salvinia.chromosomes);
+do sed 's/:/_/g' $chr.4fold.positions > tmp.$chr.4fold.positions;
+zcat geno.files/$chr.all.samples.snps.maf0.01.minInd0.1.beagle.gz | awk 'NR==FNR{a[$1]; next} $1 in a' tmp.$chr.4fold.positions -> tmp.$chr.filtered.txt;
+cat header.beagle tmp.$chr.filtered.txt |bgzip -c > geno.files/$chr.4fold.positions.maf.0.01.beagle.gz;
+done;
+
+#now merge all beagle files and remove temporary files
+cat header.beagle tmp*.filtered.txt | bgzip -c > all.chrs.4fold.positions.maf.0.01.beagle.gz
+rm tmp*
+
+#Ideally snps should be filtered by low LD for population genomic analyses, though in this case all samples seems to be a single clone with very low diversity, so we are using all snps and the 4fold sites
 #Run pcangsd with the snps dataset, testing different maf filtering is recommended;
 for maf in 0.01 0.05 0.1;
-do pcangsd -b all.samples.snps.maf0.01.minInd0.1.beagle.gz -t 8 --maf $maf -o out.pcangsd.maf$maf.minInd0.1;
+do pcangsd -b all.chrs.4fold.positions.maf.0.01.beagle.gz -t 8 --maf $maf -o out.pcangsd.4fold.sites.maf$maf.minInd0.1;
 done
 
 #Run NGSadmix
-for i in `seq 1 5`;do NGSadmix -likes all.samples.snps.maf0.01.minInd0.1.beagle.gz -K $i -outfiles out.admixure.maf0.01.minInd0.1.K$i -P 12;done
+for i in `seq 1 5`;do NGSadmix -likes all.chrs.4fold.positions.maf.0.01.beagle.gz -K $i -outfiles out.admixure.4fold.sites.maf0.01.minInd0.1.K$i -P 12;done
