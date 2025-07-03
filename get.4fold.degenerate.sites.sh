4d8b9c77d7e0dd18a046d90e8fac52465cea3e92
Fernando Hernandez <127550236+fernandoh33@users.noreply.github.com>
2025-07-03 12:28:25 -0600

    Delete get.4fold.degenerate.sites.sh

--- get.4fold.degenerate.sites.sh
+++
@@ -1,8 +0,0 @@
-annotation=reference/*gff
-reference=reference/*fasta
-
-#degeneracy annotation for coding transcripts
-degenotate/degenotate.py -a $annotation -g $reference -o out.degenotate/ -d --overwrite
-
-#extract 4-fold degenerate sites
-awk '{if($5==4) print $1":"$2}' out.degenotate/degeneracy-all-sites.bed > 4fold.positions
