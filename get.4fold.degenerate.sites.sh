df249142ec7ab211f13b9991ae8d1e1ef1cfe5ef
Fernando Hernandez <127550236+fernandoh33@users.noreply.github.com>
2025-07-03 12:23:26 -0600

    Update get.4fold.degenerate.sites.sh

--- get.4fold.degenerate.sites.sh
+++ get.4fold.degenerate.sites.sh
@@ -5,4 +5,4 @@ reference=reference/*fasta
 degenotate/degenotate.py -a $annotation -g $reference -o out.degenotate/ -d --overwrite
 
 #extract 4-fold degenerate sites
-awk '{if($5==4) print $1":"$2}' out.degenotate/degeneracy-all-sites.bed > 4-fold.sites
+awk '{if($5==4) print $1":"$2}' out.degenotate/degeneracy-all-sites.bed > 4fold.positions
