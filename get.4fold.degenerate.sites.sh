1eca1ae47937b3ed93097e6ae0c2c22d93d408e9
Fernando Hernandez <127550236+fernandoh33@users.noreply.github.com>
2025-07-03 12:21:25 -0600

    Update get.4fold.degenerate.sites.sh

--- get.4fold.degenerate.sites.sh
+++ get.4fold.degenerate.sites.sh
@@ -1,1 +1,8 @@
+annotation=reference/*gff
+reference=reference/*fasta
 
+#degeneracy annotation for coding transcripts
+degenotate/degenotate.py -a $annotation -g $reference -o out.degenotate/ -d --overwrite
+
+#extract 4-fold degenerate sites
+awk '{if($5==4) print $1":"$2}' out.degenotate/degeneracy-all-sites.bed > 4-fold.sites
